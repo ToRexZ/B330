379397ffe4561de4e96c06bfa1faa9797446edbe
Victor Risager <victor.risager@hotmail.com>
2020-10-20 20:32:54 +0200

    Added a box volume calculator

--- src/classes.cpp
+++ src/classes.cpp
@@ -1,3 +1,31 @@
-class Test
+#include <iostream>
+
+
+
+class Box
 {
+    private:
+        double width;
+        double height;
+        double length;
+
+    public:
+        double volume()
+        {
+            return width * height * length;
+        }
+        Box(double widthValue, double heightValue, double lengthValue){
+            width = widthValue;
+            height = heightValue;
+            length = lengthValue;
+        }
+
 };
+
+int main(int argc, char const *argv[])
+{
+    
+    Box myBox(1.0, 5.0, 7.0);
+    std::cout << "The volume of the box is: " << myBox.volume() << std::endl;
+    return 0;
+}
