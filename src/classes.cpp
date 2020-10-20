d238df606306c5f9442da582a6368c0499dfda26
Victor Risager <victor.risager@hotmail.com>
2020-10-20 21:08:12 +0200

    Forgot to include the classes.cpp file in the commit

--- src/classes.cpp
+++ src/classes.cpp
@@ -1,31 +1,10 @@
+#include "Box.h"
 #include <iostream>
-
-
-
-class Box
-{
-    private:
-        double width;
-        double height;
-        double length;
-
-    public:
-        double volume()
-        {
-            return width * height * length;
-        }
-        Box(double widthValue, double heightValue, double lengthValue){
-            width = widthValue;
-            height = heightValue;
-            length = lengthValue;
-        }
-
-};
 
 int main(int argc, char const *argv[])
 {
-    
-    Box myBox(1.0, 5.0, 7.0);
-    std::cout << "The volume of the box is: " << myBox.volume() << std::endl;
+    Box FirstBox(1.5, 5.3, 5.2);
+    double volume = FirstBox.Volume();
+    std::cout << "Box volume is: " << volume << std::endl;
     return 0;
 }
