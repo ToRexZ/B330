c72621866a3142dd6324c69d46722b428534d545
AlexanderFoergaardNielsen <anie20@student.aau.dk>
2020-10-21 11:09:39 +0200

    quicksort method

---
+++ src/quicksort.cpp
@@ -0,0 +1,29 @@
+void quicksort(int iList[], int first, int last){
+    
+    int pivot, i, j, tmp;
+
+    if (first<last){
+        pivot = first;
+        i= first;
+        j = last;
+
+        while(i<j) {
+            while (iList[i]<=iList[pivot] && i<last){
+                i++;
+            }
+            while (iList[j]>iList[pivot]){
+                j--;
+            }
+            if(i<j){
+                tmp = iList[i];
+                iList[i] = iList[j];
+                iList[j] = tmp;
+            }
+        }
+        tmp = iList[pivot];
+        iList[pivot] = iList[j];
+
+        iList[j] = tmp;
+        quicksort(iList, first, j-1);
+        quicksort(iList, j+1, last);
+    }
